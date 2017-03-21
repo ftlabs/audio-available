f2faeeb2e2b72da4c46987a78e35362f62345264
Sean Tracey <seanmtracey@gmail.com>
2017-03-21 12:28:46 +0000

    Adjust to follow spec, and use JavaScript classes.

--- bin/lib/healthchecks.js
+++ bin/lib/healthchecks.js
@@ -1,57 +1,64 @@
 const debug = require('debug')('bin:lib:healthchecks');
+const moment = require('moment');
 
 const database = require('./database');
 const checkAudio = require('./check-audio');
 const checkUUID = require('./check-uuid');
 
+class Check{
+
+	constructor(name, severity = 1){
+		this.name = name;
+		this.ok = false;
+		this.severity = severity;
+		this.businessImpact = "TODO";
+		this.technicalSummary = "TODO";
+		this.panicGuide = "TODO";
+		this.checkOutput = "TODO";
+		this.lastUpdated = moment(Date.now() / 1000, 'X').format('YYYY-MM-DDThh:mm:ss.sTZD');
+	}
+
+}
+
 function readFromDatabase(){
 
-	const checkDescription = {
-		name : 'database check',
-		passed : undefined
-	};
+	const thisCheck = new Check('database check');
 
 	return database.describe(process.env.AWS_AUDIO_METADATA_TABLE)
 		.then(function(){
-			checkDescription.passed = true;
-			return checkDescription;
+			thisCheck.ok = true;
+			return thisCheck;
 		})
 		.catch(err => {
 			debug(err);
-			checkDescription.passed = false;
-			checkDescription.error = err;
-			return checkDescription;
+			thisCheck.ok = false;
+			thisCheck.error = err;
+			return thisCheck;
 		})
 	;
 }
 
 function checkCanReadFromBucket(){
 	
-	const checkDescription = {
-		name : 'S3 Bucket check',
-		passed : undefined
-	};
+	const thisCheck = new Check('S3 Bucket check');
 
 	return checkAudio('ae092214-d36f-11e6-b06b-680c49b4b4c0')
 		.then(function(){
-			checkDescription.passed = true;
-			return checkDescription;
+			thisCheck.ok = true;
+			return thisCheck;
 		})
 		.catch(err => {
 			debug(err);
-			checkDescription.passed = false;
-			checkDescription.error = err;
-			return checkDescription;
+			thisCheck.ok = false;
+			thisCheck.error = err;
+			return thisCheck;
 		})
 	;
 }
 
 function checkCanValidateUUID(){
 
-	const checkDescription = {
-		name : 'validate UUID check',
-		passed : undefined
-	};
+	const thisCheck = new Check('validate UUID check');
 
 	const testUUID = '97779dfc-0e1e-11e7-b030-768954394623';
 	const falseUUID = 'not_a_valid_UUID'
@@ -64,15 +71,15 @@ function checkCanValidateUUID(){
 			if(results[0] !== true || results[1] !== false ){
 				throw `${testUUID} test should be return 'true' and is ${results[0]}. '${falseUUID}' test should return 'false' and is ${results[1]}`
 			} else {
-				checkDescription.passed = true;
-				return checkDescription;
+				thisCheck.ok = true;
+				return thisCheck;
 			}
 		})
 		.catch(err => {
 			debug(err);
-			checkDescription.passed = false;
-			checkDescription.error = err;
-			return checkDescription;
+			thisCheck.ok = false;
+			thisCheck.error = err;
+			return thisCheck;
 		})
 	;
 	
